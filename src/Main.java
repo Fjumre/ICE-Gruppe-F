5f8554560f7fd4c352c30e89cc095267d49139a1
Nikolai2025 <124783539+Nikolai2025@users.noreply.github.com>
2023-05-12 12:07:09 +0200

    ice

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,45 @@
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    static UserHandler userhandler = new UserHandler("user.txt");
+    static TextUi textUi = new TextUi(userhandler);
     public static void main(String[] args) {
-        List<User> userList = DBConnector.createUser();
+        UserHandler userhandler = new UserHandler("user.txt");
+        TextUi textUi = new TextUi(userhandler);
+        try {
+            userhandler.loadUsers();
+        } catch (Exception e) {
+            e.printStackTrace();
 
-        List<CancerInfo> cancerList = DBConnector.getCancerData();
+            System.out.println("Desværre kan vi ikke køre programmet lige nu");
+            System.exit(0);
+
+        }
+        giveUserOptions();
+    }
+
+
+    private static void giveUserOptions () {
+
+        String userInput = TextUi.getUserInput();
+        System.out.println(userInput);
+        switch (userInput) {
+            case "1":
+                textUi.loginMenu();
+                break;
+            case "2":
+                textUi.createUserMenu();
+                break;
+            default:
+                System.out.println("Wrong input, please try again");
+                giveUserOptions();
+        }
+
+
+
+    }
+
+
 
         Scanner scanner = new Scanner(System.in);
         boolean questionnaire = true;
@@ -45,4 +79,3 @@ public class Main {
 
 
     }
-}
