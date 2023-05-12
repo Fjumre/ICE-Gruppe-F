a7f3763ac6617064d7cd85ba4bf55d31427c0d52
MGold <mikkelgold@protonmail.com>
2023-05-12 11:30:53 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+
         List<User> userList = DBConnector.createUser();
+
         List<CancerInfo> cancerList = DBConnector.getCancerData();
+
     }
 }
