3d0893f64f44516cdb62d7c02fbf3bc82136901a
MGold <mikkelgold@protonmail.com>
2023-05-15 13:31:00 +0200

    Update DBConnector.java

--- src/DBConnector.java
+++ src/DBConnector.java
@@ -110,4 +110,61 @@ public class DBConnector {
         }//end try
         return ci;
     }
+    public void readName(String cancer) {
+
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        try{
+            //STEP 1: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //STEP 2: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+            //STEP 3: Execute a query
+            System.out.println("Creating statement...");
+            String sql = "SELECT name FROM ice.cancerinfo";
+            stmt = conn.prepareStatement(sql);
+
+
+
+            ResultSet rs = stmt.executeQuery();
+
+            //STEP 4: Extract data from result set
+            while(rs.next()){
+                //Retrieve by column name
+
+                String name = rs.getString( "name");
+                System.out.println(name);
+
+            }
+            //STEP 5: Clean-up environment
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    stmt.close();
+            }catch(SQLException se2){
+            }// nothing we can do
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+
+
+
+    }
 }
