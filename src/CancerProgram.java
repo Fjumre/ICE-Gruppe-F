b447911c6cd47cf2e88293b2655f583859953f34
Mattsigurd <matthias.sigurds@gmail.com>
2023-05-15 13:38:46 +0200

    update

--- src/CancerProgram.java
+++ src/CancerProgram.java
@@ -1,2 +1,13 @@
 public class CancerProgram {
+    double chanceProcent;
+
+    public searchByCancerInfo(){
+        String cancerName = SELECT name FROM ice.cancerinfo where name = "name";
+    }
+    public void searchBySymptoms(){
+        String
+    }
+    public void searchByCancerSickness(){
+
+    }
 }
