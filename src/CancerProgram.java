c459edf56e576bbf021f59adcdbce761264e543b
Mattsigurd <matthias.sigurds@gmail.com>
2023-05-16 11:07:51 +0200

    .

--- src/CancerProgram.java
+++ src/CancerProgram.java
@@ -5,7 +5,7 @@
         String cancerName = SELECT name FROM ice.cancerinfo where name = "name";
     }
     public void searchBySymptoms(){
-        String
+
     }
     public void searchByCancerSickness(){
 
